6c16aec3715144aad4cf1685d79ca19801320347
hergi2004 <35924878+hergi2004@users.noreply.github.com>
2021-08-02 21:40:06 -0500

    Update UserSignUpOperationTest.java

--- src/test/java/com/seleniumtesting/demoblaze/UserSignUpOperationTest.java
+++ src/test/java/com/seleniumtesting/demoblaze/UserSignUpOperationTest.java
@@ -31,12 +31,17 @@ public class UserSignUpOperationTest {
     @Test
     public void userLogin() throws InterruptedException
     {
-        WebElement usernameTxt = driver.findElement(By.id("username"));
-        usernameTxt.sendKeys("tomsmith");
-        WebElement passwordTxt = driver.findElement(By.id("password"));
-        passwordTxt.sendKeys("SuperSecretPassword!");
-        WebElement submitBtn = driver.findElement(By.className("radius"));
-        submitBtn.click();
+        WebDriverWait waitForLoginScreenToLoad = new WebDriverWait(driver, 10);
+        waitForLoginScreenToLoad.until(ExpectedConditions.visibilityOfElementLocated(By.id("buttonOK")));
+//         WebElement usernameTxt = driver.findElement(By.id("username"));
+//         usernameTxt.sendKeys("tomsmith");
+//         WebElement passwordTxt = driver.findElement(By.id("password"));
+//         passwordTxt.sendKeys("SuperSecretPassword!");
+//         WebElement submitBtn = driver.findElement(By.className("radius"));
+//         submitBtn.click();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.id("buttonOK")).click();
         System.out.println("Current URL is:" + driver.getCurrentUrl());
         Assert.assertTrue(driver.getCurrentUrl().contains("secure"));
     }
